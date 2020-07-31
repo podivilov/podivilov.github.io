0586fdd7dea315026f70091bd861215736422d7b
Mihail Podivilov <mihail@podivilov.ru>
2020-07-31 20:23:02 +0300

    fix

--- legacy/stub/script.js
+++
@@ -1,9 +0,0 @@
-var showText = function (target, message, index, interval) {
-  if (index < message.length) {
-    $(target).append(message[index++]);
-    setTimeout(function () { showText(target, message, index, interval); }, interval);
-  }
-}
-$(function () {
-  showText("h1", "This is msk-1.", 0, 80);
-});
